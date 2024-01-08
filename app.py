e29153714e00c382814d300bb87cf9ba9e01ff3b
Mike M <mike@mike-macbook>
2024-01-08 10:55:57 -0600

    add index.html

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -11,7 +10,7 @@ def index():
     Message = None
     if ((cpu_percent) > 80 or (mem_percent > 80)):
         Message = "High CPU or Memory Utilization detected. Please sclae up"
-    return f"CPU Utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+    return render_template("index.html", cpu_percent = cpu_percent, mem_percent = mem_percent, message = Message)
 
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
