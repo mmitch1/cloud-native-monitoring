80e00974014cd3619a999aaa0f463291c1279c58
Mike M <mike@mike-macbook>
2024-01-22 11:11:39 -0600

    push dockr image to ECR

--- ecr.py
+++ ecr.py
@@ -2,9 +2,9 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-repo_name = "cloud-native-monitor-repo"
-response = ecr_client.create_repository(repositoryName = repo_name)
+repository_name = "cloud-native-monitor-repo"
+response = ecr_client.create_repository(repositoryName = repository_name)
 
-repo_uri = response['repository']['repositoryUri']
+repository_uri = response['repository']['repositoryUri']
 
-print(repo_uri)
+print(repository_uri)
