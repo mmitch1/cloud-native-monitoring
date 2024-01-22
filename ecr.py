3e4a68246c1829e11e98107c70cdd401f1a0b1c7
Mike M <mike@mike-macbook>
2024-01-22 10:55:07 -0600

    create ecr.py

---
+++ ecr.py
@@ -0,0 +1,10 @@
+import boto3
+
+ecr_client = boto3.client('ecr')
+
+repo_name = "cloud-native-monitor-repo"
+response = ecr_client.create_repository(repositoryName = repo_name)
+
+repo_uri = response['repository']['repositoryUri']
+
+print(repo_uri)
