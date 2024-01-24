c6ad696952e42adcf4ecc8b611024ca98d22f5f9
Mike M <mike@mike-macbook>
2024-01-24 09:42:16 -0600

    finish project

--- eks.py
+++ eks.py
@@ -8,22 +8,22 @@ api_client = client.ApiClient()
 
 # The deployment
 deployment = client.V1Deployment(
-    metadata=client.V1DeploymentSpec(name="cloud-monitor-flask-app"),
+    metadata=client.V1ObjectMeta(name="cloud-monitor-flask-app"),
     spec=client.V1DeploymentSpec(
         replicas=1,
         selector=client.V1LabelSelector(
             match_labels={"app": "cloud-monitor-flask-app"}
         ),
-        template=client.V1LabelSelector(
+        template=client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(
                 labels={"app": "cloud-monitor-flask-app"}
             ),
             spec=client.V1PodSpec(
-                conntainers=[
+                containers=[
                     client.V1Container(
                         name="flask-container",
                         image="326473773557.dkr.ecr.us-west-1.amazonaws.com/cloud-native-monitor-repo:latest",
-                        ports=[client.V1ContainerPorrt(container_port=5000)]
+                        ports=[client.V1ContainerPort(container_port=5000)]
                     )
                 ]
             )
